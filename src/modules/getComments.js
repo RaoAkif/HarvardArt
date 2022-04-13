899461556ccd44305a8b99272eb2da0324a56e54
emyrue <94935633+emyrue@users.noreply.github.com>
2022-04-13 14:52:22 -0600

    Removed import statement

--- src/modules/getComments.js
+++ src/modules/getComments.js
@@ -1,8 +1,6 @@
-import endpoint from "./api.js";
-
 const displayComments = async (object) => {
   const commentList = document.querySelector('.comments-list');
-  const fetchComments = await fetch(`${endpoint.newComment}${object.title}`);
+  const fetchComments = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/4RCRuzDbyxrwwon2SF6W/comments?item_id=${object.title}`);
   fetchComments.json().then(result => {
     if (result.length > 0) {
       commentList.innerHTML = '';
